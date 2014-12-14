ccabec81f47668e22e2d6f43fd3970233c09ce27
App Academy Student <student@appacademy.io>
2014-12-14 14:25:34 -0800

    question and answers in index

--- api/controllers/QuestionController.js
+++ api/controllers/QuestionController.js
@@ -8,7 +8,17 @@
 module.exports = {
 	
 	'index': function (req, res) {
-		res.view();
+		Question.findOne(1).exec(function (err, question) {
+			if (err) return next(err);
+			
+			Question.findOne(1).populate('answers').exec(function (err, answersObj) {
+				res.view({
+					question: question,
+					answers: answersObj.answers
+				});				
+			})
+
+		});
 	}
 	
 };
