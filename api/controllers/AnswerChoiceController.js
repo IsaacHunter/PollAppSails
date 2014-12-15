caf666940d34d057a216ab76da06a4c1964e64bd
App Academy Student <student@appacademy.io>
2014-12-15 13:14:43 -0800

    Saving ip addres in database for each answer choice

--- api/controllers/AnswerChoiceController.js
+++ api/controllers/AnswerChoiceController.js
@@ -8,17 +8,34 @@
 module.exports = {
 	
 	'create': function (req, res) {
-		AnswerChoice.create({answer: req.param('answerId')}).exec(function (err, ansChoice) {
-			if (err) {
-				console.log(err);
+		var ipVoter = req.connection.remoteAddress;
+		var answerId = req.param('answerId')
+		var params = {
+			answer: answerId,
+			ip: ipVoter
+		};
+		
+		AnswerChoice.find({ ip: ipVoter }).exec(function (err, ansChoices) {
+			console.log(ansChoices);
+			if ( ansChoices.all(function (ansChoice) { return ansChoice.answer != answerId }) ) {
+				AnswerChoice.create(params).exec(function (err, ansChoice) {
+					if (err) {
+						console.log(err);
+						res.redirect('/');
+						return
+					}
+			
+					console.log("Vote accepted");
+					res.redirect('/');
+					return;
+				});				
+			} else {
+				console.log('Not accepted!');
 				res.redirect('/');
 				return
 			}
-			
-			console.log("Vote accepted");
-			res.redirect('/');
-			return;
-		})
+		});
+		
 	}
 	
 };
