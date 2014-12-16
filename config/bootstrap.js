36287157b82b64dbbd62537fb8b6f44df0eb7e6d
App Academy Student <student@appacademy.io>
2014-12-15 18:10:34 -0800

    Don't create seed data if it's already there

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -14,27 +14,30 @@ module.exports.bootstrap = function(cb) {
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
 	
-	
-	User.create({email:"email@email.com", password:"password"}).exec(function (err, user) {
-		Question.create({user: user.id, title:"Is Austin the coolest city ever?"}).exec(function (err, question) {
-			Answer.create({question: question.id, title:"YEEAAHH"}).exec(function (err, answer) {
-				Answer.create({question: question.id, title:"Hella no! San Francisco rules!"}).exec(function (err, answer) {					
+	User.find().exec( function (err, users) {
+		if (users.length === 0) {
+			User.create({email:"email@email.com", password:"password"}).exec(function (err, user) {
+				Question.create({user: user.id, title:"Is Austin the coolest city ever?"}).exec(function (err, question) {
+					Answer.create({question: question.id, title:"YEEAAHH"}).exec(function (err, answer) {
+						Answer.create({question: question.id, title:"Hella no! San Francisco rules!"}).exec(function (err, answer) {					
+						});
+					});
+				});
+				Question.create({user: user.id, title:"What's your favorite food?"}).exec(function (err, question) {
+					Answer.create({question: question.id, title:"Tacos"}).exec(function (err, answer) {
+						Answer.create({question: question.id, title:"Tacos al pastor!"}).exec(function (err, answer) {					
+						});
+					});
+				});
+				Question.create({user: user.id, title:"Majorca or Austin??"}).exec(function (err, question) {
+					Answer.create({question: question.id, title:"None! San Francisco!"}).exec(function (err, answer) {
+						Answer.create({question: question.id, title:"They are both Awesome!"}).exec(function (err, answer) {					
+						});
+					});
 				});
 			});
-		});
-		Question.create({user: user.id, title:"What's your favorite food?"}).exec(function (err, question) {
-			Answer.create({question: question.id, title:"Tacos"}).exec(function (err, answer) {
-				Answer.create({question: question.id, title:"Tacos al pastor!"}).exec(function (err, answer) {					
-				});
-			});
-		});
-		Question.create({user: user.id, title:"Majorca or Austin??"}).exec(function (err, question) {
-			Answer.create({question: question.id, title:"None! San Francisco!"}).exec(function (err, answer) {
-				Answer.create({question: question.id, title:"They are both Awesome!"}).exec(function (err, answer) {					
-				});
-			});
-		});
-	});
+		}
+	})
 	
 	cb();
 };
